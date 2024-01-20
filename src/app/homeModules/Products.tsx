a01ed6af096f04d0bd097eb984b70f222696f767
princeemeka965 <anyanwue4@gmail.com>
2024-01-20 13:16:20 +0100

    re-work done on pagination feature

--- src/app/homeModules/Products.tsx
+++ src/app/homeModules/Products.tsx
@@ -12,6 +12,7 @@ import { Button } from "@material-tailwind/react";
 const Products: React.FC = () => {
   const [itemsPerPage] = useState<number>(20);
   const [pageNo, setPageNo] = useState<number>(0);
+  const [paginateLoader, setLoader] = useState<boolean>(false);
 
   const products = useSelector(
     (state: RootState) => state.productsData.products
@@ -33,16 +34,25 @@ const Products: React.FC = () => {
     const productsData =
       pageNo > 0 ? products.concat(data?.products) : data?.products;
 
+    // set pagination loader to false
+    setLoader(false);
+
     // dispatch our products data to the store
     dispatch(SET_PRODUCTS(productsData));
   }, [data, dispatch]);
+
+  const nextPage = () => {
+    setPageNo(pageNo + 1);
+    // set pagination loader state to true
+    setLoader(true);
+  };
 
   return (
     <>
       <div className="lg:py-20 md:py-20 py-5 lg:px-24 px-4 w-full h-auto flex flex-col">
         <div className="w-full flex justify-center">
           <div className="lg:w-3/5 md:w-3/5 w-4/5 flex flex-col gap-3">
-            <span className="text-xl text-center lg:flex md:flex hidden text-secondaryGrey">
+            <span className="text-xl text-center justify-center lg:flex md:flex hidden text-secondaryGrey">
               Featured Products
             </span>
             <span className="text-2xl font-bold text-center text-meshBlack uppercase">
@@ -56,21 +66,27 @@ const Products: React.FC = () => {
 
         <div className="w-full flex flex-row flex-wrap px-3 py-20 items-center gap-x-1 gap-y-10 justify-between">
           {isLoading ? (
-            <Loader />
+            <Loader
+              size="
+            lg"
+            />
           ) : (
             <>
-              {products ? (
-                <ProductsList products={products} />
-              ) : products?.length === data?.total ? null : (
+              {products ? <ProductsList products={products} /> : null}
+              {products?.length === data?.total ? null : (
                 <div className="w-full my-8 flex justify-center">
-                  <Button
-                    variant="outlined"
-                    placeholder=""
-                    className="uppercase text-primaryBlue border rounded-md border-primaryBlue"
-                    onClick={() => setPageNo(pageNo + 1)}
-                  >
-                    Load More Products
-                  </Button>
+                  {paginateLoader ? (
+                    <Loader size="xs" />
+                  ) : (
+                    <Button
+                      variant="outlined"
+                      placeholder=""
+                      className="uppercase text-primaryBlue border rounded-md border-primaryBlue"
+                      onClick={() => nextPage()}
+                    >
+                      Load More Products
+                    </Button>
+                  )}
                 </div>
               )}
             </>
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,21 @@
-export default function Loader() {
+interface LoaderProps {
+  size: string;
+}
+
+const Loader: React.FC<LoaderProps> = (props) => {
   return (
     <>
       <div className="w-full h-full flex relative py-20">
         <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center text-center">
-          <div className="custom-loader" />
+          {props.size === "xs" ? (
+            <div className="custom-loader-xs" />
+          ) : (
+            <div className="custom-loader" />
+          )}
         </div>
       </div>
     </>
   );
-}
+};
+
+export default Loader;
